f136609bb590fd58d5374e4e3b5ce084857c23cf
2joopiter <veryberrylike3@gmail.com>
2023-11-13 19:45:09 +0900

    mission4

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ const box = document.querySelector('.box');
 const colors = ['hotpink', 'royalblue', 'orange'];
 
 btns.forEach((bt, idx) => {
-	bt.addEventListener('click', () => {
+	bt.addEventListener('click', (evt) => {
+		if (evt.currentTarget.classList.contains('on')) return;
 		btns.forEach((bt) => bt.classList.remove('on'));
 		btns[idx].classList.add('on');
 		box.style.backgroundColor = colors[idx];
