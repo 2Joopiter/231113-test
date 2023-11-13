93ae12572f5485fe888490773d5f47993bc4160b
2joopiter <veryberrylike3@gmail.com>
2023-11-13 19:33:28 +0900

    mission2

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 const btns = document.querySelectorAll('li');
+const box = document.querySelector('.box');
+const colors = ['hotpink', 'royalblue', 'orange'];
 
 btns.forEach((bt, idx) => {
 	bt.addEventListener('click', () => {
 		btns.forEach((bt) => bt.classList.remove('on'));
 		btns[idx].classList.add('on');
+		box.style.backgroundColor = colors[idx];
 	});
 });
